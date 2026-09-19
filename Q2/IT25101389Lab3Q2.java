801c13d5c0c6cc94cb9b6d14f8f9f0120da5d8ff
Ul Hassan Q.S <IT25101389@my.sliit.lk>
2026-09-19 16:14:21 +0530

    Add files via upload

---
+++ Q2/IT25101389Lab3Q2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IT25101389Lab3Q2{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the monthly salary: ");
+		double monthlySalary = input.nextDouble();
+		
+		System.out.print("Enter the number of OT hours: ");
+		double otHours = input.nextDouble();
+		
+		System.out.print("Enter the OT hourly rate: ");
+		double otRate = input.nextDouble();
+		
+		double otAmount = otHours * otRate;
+		double totalSalary = monthlySalary + otAmount;
+		
+		System.out.println("The total salary including OT is: " + totalSalary);
+	}
+}
